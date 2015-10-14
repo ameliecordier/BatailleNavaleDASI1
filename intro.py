ed398fe300f93b562f7d03c5ee861afb691a97f9
acordier <acordier@gmail.com>
2015-10-14 11:02:57 +0200

    Création d'une méthode d'accueil et génération de la grille.

--- intro.py
+++ intro.py
@@ -1,8 +1,23 @@
-print("Bonjour et bienvenue dans ce jeu de bataille navale merveilleux !")
-print("Voulez-vous jouer avec moi ?")
-jouer = input("Répondez O pour oui, N pour non... ")
+from pprint import pprint
+def start_up(self):
+    print("Bonjour et bienvenue dans ce jeu de bataille navale merveilleux !")
+    print("Voulez-vous jouer avec moi ?")
+    jouer = input("Répondez O pour oui, N pour non... ")
 
-if jouer =="O":
-    print("Yeahhh !")
-else:
-    print("Ohhh nonnnnn")
+    if jouer =="O":
+        print("Yeahhh !")
+    else:
+        print("Ohhh nonnnnn")
+
+print("Indiquer la taille de l'océan :")
+largeur_ocean = int(input("Quelle est la largeur de l'océan : "))
+hauteur_ocean = int(input("Quelle est la hauteur de l'océan : "))
+
+ocean = []
+for liste in range(hauteur_ocean):
+    ligne = []
+    for case in range(largeur_ocean):
+        ligne.append('O')
+    ocean.append(ligne)
+pprint(ocean)
+     
