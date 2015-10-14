ca6a9f45872fcb898992bf2a92da042c7c2f3701
acordier <acordier@gmail.com>
2015-10-14 10:39:28 +0200

    Réponse à la question 1 par Amélie

--- intro.py
+++ intro.py
@@ -1,1 +1,8 @@
-print("Hello world")
+print("Bonjour et bienvenue dans ce jeu de bataille navale merveilleux !")
+print("Voulez-vous jouer avec moi ?")
+jouer = input("Répondez O pour oui, N pour non... ")
+
+if jouer =="O":
+    print("Yeahhh !")
+else:
+    print("Ohhh nonnnnn")
