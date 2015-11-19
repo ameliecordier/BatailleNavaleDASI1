6267690215f9ac8db8003ae51a937a0b69ebbff4
acordier <acordier@gmail.com>
2015-11-19 14:14:11 +0100

    Petit refactoring

--- bateau.py
+++ bateau.py
@@ -1,13 +1,15 @@
 types_bateau = {
     'porte-avion': 5,
-	'croiseur': 4,
-	'sous-marin': 3,
-	'contre-torpilleur': 3,
-	'torpilleur': 2
+    'croiseur': 4,
+    'sous-marin': 3,
+    'contre-torpilleur': 3,
+    'torpilleur': 2
 }
 
 class Bateau:
     def __init__(self, type):
-	    self.type = type
-		self.taille = types_bateau[type]
-		self.is_placed = False
+        self.type = type
+        self.taille = types_bateau[type]
+	self.is_placed = False
+
+
--- test_bateau.py
+++ test_bateau.py
@@ -8,3 +8,42 @@ class TestBateau(unittest.TestCase):
 		
     def test_not_a_boat(self):
 	    self.assertRaises(KeyError, Bateau, 'pas_un_bateau')
+
+
+### Test créer bateau
+##a = creerBateau("Porte-Avion")
+##assertEqual(a.longueur, 5)
+##
+##b = creerBateau("Croiseur")
+##assertEqual(b.longueur, 4)
+##
+##c = creerBateau("Radeau")
+##assertEqual(c.longueur, 1)
+##
+##d = creerBateau("Tank")
+##assertRaises(ValueError)
+##
+##e = creerBateau(42)
+##assertRaises(TypeError)
+##
+##
+### Test ajouterBateaux
+##
+### Test 1
+##armada = []
+##monBateau = creerBateau("Porte-Avion")
+##armada.ajouterBateau(monBateau)
+##assertEqual(armada.len, 1)
+##
+### Test 2
+##armada = []
+##monBateau = creerBateau("Porte-Avion")
+##armada.ajouterBateau(monBateau)
+##assertEqual(armada.len, 1)
+##monAutreBateau = creerBateau("Porte-Avion")
+##armada.ajouterBateau(monAutreBateau)
+##assertEqual(armada.len, 2)
+##
+##
+###... et quoi d'autre ? 
+##
