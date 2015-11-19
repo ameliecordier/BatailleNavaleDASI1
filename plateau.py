779781381746ed1da3ab44593c6c7a5a7d5f0029
RPoulalier <remy.poulalier@etu.univ-lyon1.fr>
2015-11-19 15:49:33 +0100

    Plateau --- Rémy, Maxence, Nicolas, Félix

---
+++ plateau.py
@@ -0,0 +1,14 @@
+from intro import Fonctions
+
+class Plateau:
+    def __init__(self, tailleoceanx, tailleoceany):
+        
+        #this.ocean1 = Fonctions.construire_ocean(tailleoceanx,tailleoceany)
+        #this.flotte1 = Fonctions.creeFlotte()
+        
+        
+        #this.ocean2 = Fonctions.construire_ocean(tailleoceanx,tailleoceany)
+        #this.flotte2 = Fonctions.creeFlotte()
+
+    def jouer(self):
+        #boucle tant que personne n'a pas gagné
