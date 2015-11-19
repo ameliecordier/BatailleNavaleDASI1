37263a6a5e5d7c338fcd75a38af113e8601cc670
Quentin <quentin.vaudaine@gmail.com>
2015-11-19 15:52:30 +0100

    Creation de la classe Ocean

--- intro.py
+++ intro.py
@@ -11,26 +11,3 @@ class Fonctions:
             print("Yeahhh !")
         else:
             print("Ohhh nonnnnn")
-
-    def saisir_params_ocean():
-        print("Notre plus grand océan est de taille 10 * 10")
-        print("Indiquer la taille de l'océan :")
-        largeur = int(input("Quelle est la largeur de l'océan : "))
-        hauteur = int(input("Quelle est la hauteur de l'océan : "))
-        return hauteur, largeur
-
-    def construire_ocean(hauteur_ocean, largeur_ocean):
-        ocean = []
-        for liste in range(hauteur_ocean):
-            ligne = []
-            for case in range(largeur_ocean):
-                ligne.append('O')
-            ocean.append(ligne)
-
-        print("len(ocean) :" + str(len(ocean)))
-        print("len(ocean[0]) :" + str(len(ocean[0])))
-
-        return ocean
-
-    def afficher_ocean(ocean):
-        pprint(ocean)
