aa9bd2f1bb9c0bc84354e4b0265d7848d6cab491
FeL <felix.laube@etu.univ-lyon1.fr>
2015-12-01 15:18:47 +0100

    Affichage des deux océans

--- plateau.py
+++ plateau.py
@@ -6,7 +6,7 @@ class Joueur:
     def __init__(self, tailleoceanx, tailleoceany):
         
         ocean1 = Ocean(tailleoceanx,tailleoceany)
-        #this.flotte1 = Fonctions.creeFlotte()
+        #flotte1 = Fonctions.creeFlotte()
         
     def jouer(self, position):
         #parcours de la liste des bateaux adverses pour voir si touché
@@ -15,7 +15,21 @@ class Joueur:
         
         
     def afficherAdversaire(self):
-        #afficher tir sur océan adverse
+        #afficher océan connu de l'adversaire
+        ocean1.afficher_ocean()
 
-    def afficherOcean(self):
+    def afficherSoi(self):
         #afficher océan du joueur actif
+        ocean_temporaire = Ocean(tailleoceanx,tailleoceany)
+
+        #for bateau in flotte1:
+            #i est un bâteau
+         #   for ObjetPos in bateau.listepos:
+          #      if ObjetPos.etat:
+           #         ocean_temporaire[ObjetPos.x][ObjetPos.y] = '\'
+            #    else:
+             #       ocean_temporaire[ObjetPos.x][ObjetPos.y] = '€'
+                    
+        ocean_temporaire.afficher_ocean()
+                    
+        
