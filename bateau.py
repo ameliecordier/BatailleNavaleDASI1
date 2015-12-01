eb55defcc64ce8c38e184dd73f43c2f0decc6ec9
ngarcia <Administrateur@UC2515.cha.sihc.local>
2015-12-01 15:48:02 +0100

    modification de la fonction set_position

--- bateau.py
+++ bateau.py
@@ -9,7 +9,6 @@ types_bateau = {
     'torpilleur': 2
 }
 
-
 class Bateau:
     def __init__(self, type):
         self.type = type
@@ -22,22 +21,42 @@ class Bateau:
     '''
     Positionne un bateau sur l'océan.
     '''
-        
+        messOk = "Le bateau a bien été placé."
+        messErr = "Le bateau n'est pas sur le plateau!"
         if x >= 0 or y >= 0 or x <=  or y <= 10:
-             
+            
             if orient == 'haut':
                 if (y-self.taille)>=0:
                     for i in range(0,self.taille-1) 
                         if ocean.getStatutCase(x,y-i):
-                            self.position = {{x,y,orient}}
-                            print("Le bateau a bien été placé.")
+                            self.position = {{x,y-i,e}}
+                            print(messOk)
                 else:
-                    print("Le bateau n'est pas sur le plateau!")
+                    print(messErr)
             elif orient == 'bas':
-                
+                if (y+self.taille)>=0:
+                        for i in range(0,self.taille-1) 
+                            if ocean.getStatutCase(x,y+i):
+                                self.position = {{x,y+i,e}}
+                                print(messOk)
+                    else:
+                        print(messErr)    
             elif orient == 'gauche':
+                if (x-self.taille)>=0:
+                    for i in range(0,self.taille-1) 
+                        if ocean.getStatutCase(x-i,y):
+                            self.position = {{x-i,y,e}}
+                            print(messOk)
+                else:
+                    print(messErr)
             else:
-                
+                if (x+self.taille)>=0:
+                    for i in range(0,self.taille-1) 
+                        if ocean.getStatutCase(x+i,y):
+                            self.position = {{x+i,y,e}}
+                            print(messOk)
+                else:
+                    print(messErr)
             self.is_placed = True
         else:
             print("Erreur de coordonées!")
