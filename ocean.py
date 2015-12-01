87701f84f351fb7fabdd00515da299e38f9b50df
Quentin <quentin.vaudaine@gmail.com>
2015-12-01 15:52:53 +0100

    ocean.afficher_ocean() non terminée

--- ocean.py
+++ ocean.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 __author__ = 'phylante'
 from pprint import pprint
 
@@ -10,18 +12,20 @@ class Ocean:
     def __len__(self):
         return len(self.ocean)
 
-    def __getitem__(self, pos):
+    def __getitem__(self, coords):
         """
         Permet d'adresser une case de l'ocean.
         Permet de demander une case avec la formulation ligne[pos]
-        :param pos: selectionne la case de la ligne
-        :return: la case pos de la ligne passée
+        :param coords: un tuple style (A, 8)
+        utilisation : ocean[('A', 8)]
         """
-        return self.ocean[pos]
+        lettre = coords[0]
+        chiffre = coords[1]
+        return self.ocean[lettre][chiffre]
 
     def largeur(self):
         if len(self.ocean) > 0:
-            return len(self.ocean[0])
+            return len(self.ocean['A'])
 
     @staticmethod
     def saisir_params_ocean():
@@ -34,8 +38,17 @@ class Ocean:
             return Ocean.saisir_params_ocean()
 
     def construire_ocean(self, hauteur_ocean, largeur_ocean):
-        return [['O' for x in range(largeur_ocean)] for y in range(hauteur_ocean)]
+        return [OrderedDict({chr(y + 65): ['O' for x in range(largeur_ocean)] for y in range(hauteur_ocean)})]
 
     def afficher_ocean(self):
-        for x in range(len(self.ocean)):
-            print(" ".join(self.ocean[x]))
+        # On affiche les chiffres.
+        for x in range(self.largeur() + 1):
+            if x == 0:
+                print("   ", end="")
+            print(x, end=" ")
+
+        # On affiche chaque ligne précédée de sa lettre.
+        for key, ligne in self.ocean.items():
+            ligne_a_afficher = key + " " + " ".join(ligne)
+            print(ligne_a_afficher)
+
