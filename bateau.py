7bf157ad2e134bb0fd103c76ee7b5ff534a469e9
ngarcia <Administrateur@UC2515.cha.sihc.local>
2015-12-02 11:59:52 +0100

    Correction de bug dans la classe bateau

--- bateau.py
+++ bateau.py
@@ -1,5 +1,5 @@
 import ocean
-import player
+import joueur
 
 types_bateau = {
     'porte-avion': 5,
@@ -16,21 +16,20 @@ class Bateau:
         self.is_placed = False
         self.position = {}
         self.etat = True
-        static self.bateaux = {}
-
-    def set_position(self, x, y, orient, maxX, maxY):
+        self.bateaux = {}
     '''
     Vérifie que les coordonées sont juste.
     Positionne un bateau sur l'océan.
     '''
-        messOk = "Le bateau a bien été placé."
-        messErr = "Le bateau n'est pas sur le plateau!"
+    def set_position(self, x, y, orient, maxX, maxY):
+        messOk = "Le bateau a bien ete place."
+        messErr = "Le bateau n est pas sur le plateau!" 
         if x >= 0 or y >= 0 or x <= maxX or y <= maxY:
             
             if orient == 'haut':
                 '''Test si le bateau ne sort pas de plateau  '''
                 if (y-self.taille)>=0:
-                    for i in range(0,self.taille-1) 
+                    for i in range(0,self.taille-1): 
                         if self.getStatutCase(x,y-i):
                             self.position.append({x,y-i,e})
                             print(messOk)
@@ -38,7 +37,7 @@ class Bateau:
                     print(messErr)
             elif orient == 'bas':
                 if (y+self.taille)>=0:
-                    for i in range(0,self.taille-1) 
+                    for i in range(0,self.taille-1):
                         if self.getStatutCase(x,y+i):
                             self.position.append({x,y+i,e})
                             print(messOk)
@@ -46,7 +45,7 @@ class Bateau:
                     print(messErr)    
             elif orient == 'gauche':
                 if (x-self.taille)>=0:
-                    for i in range(0,self.taille-1) 
+                    for i in range(0,self.taille-1): 
                         if self.getStatutCase(x-i,y):
                             self.position.append({x-i,y,e})
                             print(messOk)
@@ -54,7 +53,7 @@ class Bateau:
                     print(messErr)
             else:
                 if (x+self.taille)>=0:
-                    for i in range(0,self.taille-1) 
+                    for i in range(0,self.taille-1): 
                         if self.getStatutCase(x+i,y):
                             self.position.append({x+i,y,e})
                             print(messOk)
@@ -63,7 +62,7 @@ class Bateau:
             self.bateaux.append(self.position)
             self.is_placed = True
         else:
-            print("Erreur de coordonées!")
+            print("Erreur de coordonees!")
 
     def getStatutCase(self, x, y):
         return True
