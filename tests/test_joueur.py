38b926607bd14c1326f74b32a8955411fdde7cff
Orkad <p1513002@IUTDOUAB2PC3.univ-lyon1.fr>
2015-12-04 11:52:29 +0100

    test_joueur refactoring (setUp)

--- tests/test_joueur.py
+++ tests/test_joueur.py
@@ -2,8 +2,28 @@ from joueur import *
 from bateau import *
 import unittest
 
-
 class TestJoueur(unittest.TestCase):
+    def setUp(self):
+        #definition de la taille des océans
+        self.tailleX = 5
+        self.tailleY = 5
+        
+        #Création du porte avion pour le joueur 1
+        self.porteAvionJ1 = Bateau("porte-avion")
+        self.porteAvionJ1.set_position(1,1,'haut',self.tailleX,self.tailleY)
+        self.bateauxJ1 = []
+        self.bateauxJ1.append(self.porteAvionJ1)
+
+        #Création du porte avion pour le joueur 2
+        self.porteAvionJ2 = Bateau("porte-avion")
+        self.porteAvionJ2.set_position(1,1,'haut',self.tailleX,self.tailleY)
+        self.bateauxJ2 = []
+        self.bateauxJ2.append(self.porteAvionJ2)
+
+        #Création des joueurs
+        self.J1 = Joueur(self.tailleX, self.tailleY, self.bateauxJ1)
+        self.J2 = Joueur(self.tailleX, self.tailleY, self.bateauxJ2)
+        
     #def test_joueur(self):
         
         #bateaux = []
@@ -15,32 +35,11 @@ class TestJoueur(unittest.TestCase):
         #joueur = Joueur(10,10, bateaux)
 
     def test_bateau_touche(self):
-    
-        #definition de la taille des océans
-        tailleX = 5
-        tailleY = 5
-        
-        #Création du porte avion pour le joueur 1
-        porteAvionJ1 = Bateau("porte-avion")
-        porteAvionJ1.set_position(1,1,'haut',tailleX,tailleY)
-        bateauxJ1 = []
-        bateauxJ1.append(porteAvionJ1)
-
-        #Création du porte avion pour le joueur 2
-        porteAvionJ2 = Bateau("porte-avion")
-        porteAvionJ2.set_position(1,1,'haut',tailleX,tailleY)
-        bateauxJ2 = []
-        bateauxJ2.append(porteAvionJ2)
-
-        #Création des joueurs
-        J1 = Joueur(tailleX, tailleY, bateauxJ1)
-        J2 = Joueur(tailleX, tailleY, bateauxJ2)
 
         #Tir du joueur 1 sur le joueur 2
-        J1.tirer({1,1},J2)
+        self.J1.tirer({1,1},J2)
 
         #Test sur l'unique bateau du joueur 2 sur sa première position
         #il doit etre touché
-        self.assertEqual(J2.flotte[0].position[0][2], True)
+        self.assertEqual(self.J2.flotte[0].position[0][2], True)
 
-        
